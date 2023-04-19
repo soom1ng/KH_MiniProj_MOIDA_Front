4e6dcab3625c9ccd965d9076f76183506f3d3c21
hov-i <dbsghdql55555@gmail.com>
2023-04-19 14:05:57 +0900

    jsx수정

--- src/styles/StyledComponent.js
+++ src/styles/StyledComponent.js
@@ -1,4 +1,33 @@
 import { Form } from "react-router-dom";
 import styled, {css} from 'styled-components';
 
-const 
+const bigTitle = styled.text`
+    font-size: 32px;
+    color: ${props => props.color || 'black'};
+    font-weight: bolder;
+`;
+
+const smaillTitle = styled.text`
+    font-size: 18px;
+    color: ${props => props.color || 'black'};
+    font-weight: bolder;
+`;
+
+const search = styled.div`
+    font-size:16px;
+    color: rgb(127, 127, 127);
+    background-color : rgb(239, 239, 239);
+    border-radius: 4px;
+`;
+
+const button = styled.button`
+    font-size:19px;
+    color: white;
+    background-color: rgb(107, 78, 254);
+    border-radius: 4px;
+`;
+const blockBox = styled.div`
+    background-color : white;
+    border-radius: 20px
+    
+`
